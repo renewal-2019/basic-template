c42f9e90a60a31208f8921c787f674aa1797f309
renewal-2019 <swiftzsl@163.com>
2021-07-25 10:48:13 +0800

    测试hello

--- src/main/java/com/zsl/template/TemplateApplication.java
+++ src/main/java/com/zsl/template/TemplateApplication.java
@@ -3,6 +3,12 @@ package com.zsl.template;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Hello
+ *
+ * @author swiftzsl
+ * @date 2021/7/25 10:42
+ */
 @SpringBootApplication
 public class TemplateApplication {
 
---
+++ src/main/java/com/zsl/template/controller/Hello.java
@@ -0,0 +1,20 @@
+package com.zsl.template.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello
+ *
+ * @author swiftzsl
+ * @date 2021/7/25 10:42
+ */
+@RestController
+@RequestMapping("/test")
+public class Hello {
+    @GetMapping("hello")
+    public String hello(){
+        return "hello";
+    }
+}
