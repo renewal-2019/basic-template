387cac005ca3beb1c75117041545feb6c0eb78f0
renewal-2019 <swiftzsl@163.com>
2022-07-03 14:21:59 +0800

    RabbitMQ : 高级发布确认(回退消息)

--- src/main/java/com/zsl/template/rabbitmq/高级发布确认/MyCallBack.java
+++ src/main/java/com/zsl/template/rabbitmq/高级发布确认/MyCallBack.java
@@ -1,6 +1,7 @@
 package com.zsl.template.rabbitmq.高级发布确认;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.ReturnedMessage;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,22 +11,24 @@ import javax.annotation.PostConstruct;
 
 @Component
 @Slf4j
-public class MyCallBack implements RabbitTemplate.ConfirmCallback {
+public class MyCallBack implements RabbitTemplate.ConfirmCallback, RabbitTemplate.ReturnsCallback {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
     // 因为实现的是内部的接口，所以必须要注入才能真正的生效
     @PostConstruct
-    public void init(){
+    public void init() {
         rabbitTemplate.setConfirmCallback(this);
+        rabbitTemplate.setMandatory(true);
+        rabbitTemplate.setReturnsCallback(this);
     }
 
     /**
      * 交换机确认回调方法
      *
      * @param correlationData 保存回调消息的id及相关信息，此回调内容由发送方给出
-     * @param b true： 交换机收到消息 false：交换机接收失败
-     * @param s 失败的原因
+     * @param b               true： 交换机收到消息 false：交换机接收失败
+     * @param s               失败的原因
      */
     @Override
     public void confirm(CorrelationData correlationData, boolean b, String s) {
@@ -35,5 +38,16 @@ public class MyCallBack implements RabbitTemplate.ConfirmCallback {
         } else {
             log.info("交换机还未收到 id 为:{}消息,由于原因:{}", id, s);
         }
+    }
+
+    /**
+     * 交换机无法路由时的回调方法
+     *
+     * @param returnedMessage
+     */
+    @Override
+    public void returnedMessage(ReturnedMessage returnedMessage) {
+        log.warn(" 消 息 {}, 被交换机 {} 退回，退回原因 :{}, 路 由 key:{}", returnedMessage.getMessage().getBody(),
+                returnedMessage.getExchange(), returnedMessage.getReplyText(), returnedMessage.getRoutingKey());
     }
 }
